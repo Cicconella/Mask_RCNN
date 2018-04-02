bcfc78e3190482f49d54e60a85958a0c4e173f54
ludygrv17 <ludy17ludy@gmail.com>
2018-04-02 10:50:52 -0300

    train 3+

--- samples/balloon/balloon.py
+++ samples/balloon/balloon.py
@@ -281,7 +281,7 @@ def train(model):
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs= config.EPOCHS,
-                layers='5+')
+                layers='3+')
 
 def test(model):
     """Train the model."""
