8a26346594238257a514b67c6aeb8e8423b03442
ludygrv17 <ludy17ludy@gmail.com>
2018-04-06 08:58:34 -0300

    find tensorflow

--- samples/balloon/balloon.py
+++ samples/balloon/balloon.py
@@ -26,7 +26,11 @@ Usage: import the module (see Jupyter notebooks for examples), or run from
     # Apply color splash to video using the last weights you trained
     python3 balloon.py splash --weights=last --video=<URL or path to file>
 """
+import sys
+HOME_DIR = '/home/cicconella'
+sys.path.append(HOME_DIR)
 import tensorflow as tf
+
 import os
 import sys
 import json
