876b2316b9268b9b0ca0090d859fd9f1cd894eb2
ludygrv17 <ludy17ludy@gmail.com>
2018-04-06 08:18:13 -0300

    small test

--- samples/balloon/balloon.py
+++ samples/balloon/balloon.py
@@ -83,8 +83,8 @@ class DSBConfig(Config):
     NAME = "DSB"
     IMAGES_PER_GPU = 2
     NUM_CLASSES = 2
-    STEPS_PER_EPOCH = 200
-    EPOCHS = 100
+    STEPS_PER_EPOCH = 20
+    EPOCHS = 4
     DETECTION_MIN_CONFIDENCE = 0.7
 
 ############################################################
